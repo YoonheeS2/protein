a6c8813aa2e4b415157698d048d91c3e04e4462b
5972hxx <yunhuigim99@gmail.com>
2023-09-23 21:57:37 +0900

    리액트 처음 구축하는 작업

---
+++ example/ex1.js
@@ -0,0 +1,7 @@
+let a = "김윤희" ;
+const t = "관우쨩" ;
+
+a = "유니쿤" ;
+
+console.log(a);
+console.log(t);
---
+++ example/ex3.js
@@ -0,0 +1,13 @@
+let car = {
+    name : "Sonata",
+    ph : "500ph",
+    start : function () {
+        console.log("engine is starting")
+    },
+    stop : function () {
+        console.log("engine is stoped")
+    },
+};
+
+console.log(car.name);
+car.start();
